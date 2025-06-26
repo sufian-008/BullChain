0ae5c1c7a3f665134e746339ff6bf83c61c39889
sufian008 <m.a.sufian008@gmail.com>
2025-06-26 20:14:33 +0600

    Support Page is completed

---
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+export default function CreateTicket() {
+  return (
+    <div className='container mt-5'>
+      
+      <div className='row'>
+        <h1 className='text-muted fs-5 ps-5' >To create a ticket, select a relevant topic</h1>
+        <div className='col-4 p-5 mt-2 mb-5'>
+          <h4 className='fs-5 '><i class="fa fa-plus-circle" aria-hidden="true"></i> Account Opening</h4>
+          <a href='' className='ps-4' style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Resident individual</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Minor</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Non Resident Indian (NRI)</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Company, Partnership, HUF and LLP</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Glossary</a>
+        </div>
+        <div className='col-4 p-5 mt-2 mb-5'>
+          <h4 className='fs-5 '><i class="fa fa-plus-circle" aria-hidden="true"></i> Account Opening</h4>
+          <a href='' className='ps-4' style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Resident individual</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Minor</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Non Resident Indian (NRI)</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Company, Partnership, HUF and LLP</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Glossary</a>
+        </div>
+       <div className='col-4 p-5 mt-2 mb-5'>
+          <h4 className='fs-5 '><i class="fa fa-plus-circle" aria-hidden="true"></i> Account Opening</h4>
+          <a href='' className='ps-4' style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Resident individual</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Minor</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Non Resident Indian (NRI)</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Company, Partnership, HUF and LLP</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Glossary</a>
+        </div>
+        <div className='col-4 p-5 mt-2 mb-5'>
+          <h4 className='fs-5 '><i class="fa fa-plus-circle" aria-hidden="true"></i> Account Opening</h4>
+          <a href='' className='ps-4' style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Resident individual</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Minor</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Non Resident Indian (NRI)</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Company, Partnership, HUF and LLP</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Glossary</a>
+        </div>
+        <div className='col-4 p-5 mt-2 mb-5'>
+          <h4 className='fs-5 '><i class="fa fa-plus-circle" aria-hidden="true"></i> Account Opening</h4>
+          <a href='' className='ps-4' style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Resident individual</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Minor</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Non Resident Indian (NRI)</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Company, Partnership, HUF and LLP</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Glossary</a>
+        </div>
+        <div className='col-4 p-5 mt-2 mb-5'>
+          <h4 className='fs-5 '><i class="fa fa-plus-circle" aria-hidden="true"></i> Account Opening</h4>
+          <a href='' className='ps-4' style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Resident individual</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Minor</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Non Resident Indian (NRI)</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Company, Partnership, HUF and LLP</a><br/>
+          <a href=''  className='ps-4'style={{color:"rgb(7, 129, 205)", lineHeight:"1.8"}}>Glossary</a>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -2,8 +2,31 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>
-      
-    </div>
+    <section className='container-fluid' id='supportHero'>
+      <div className=' p-5' id='supportWrapper'>
+        <h4 >Support Portal</h4>
+        <a href='' style={{ textDecoration: "underline", color: "white" }}>Track Tickets</a>
+      </div>
+      <div className='row p-3 mx-5'>
+        <div className='col-6 p-5  '>
+          <h4 className='fs-3'>Search for an answer or browse help topics to create a ticket</h4>
+          <input className='mb-2' placeholder='Eg. how do i activate F&O, why is my order getting rejected....' />
+          <br />
+          <a href='' style={{ textDecoration: "underline", color: "white", marginRight: "20px" }}>Track account opening</a>
+          <a href='' style={{ textDecoration: "underline", color: "white", marginRight: "20px" }}>Track segment activation</a>
+          <a href='' style={{ textDecoration: "underline", color: "white", marginRight: "20px" }}>Intraday margins</a>
+          <a href='' style={{ textDecoration: "underline", color: "white" }}>Kite user manual</a>
+        </div>
+        <div className='col-6  p-5 '>
+          <h4 className='fs-3'>Feature</h4>
+          <ol>
+            <li> <a href='' style={{ textDecoration: "underline", color: "white" }}>Exclusion of F&O contracts on 8 securities from August 29, 2025</a></li>
+            <li><a href='' style={{ textDecoration: "underline", color: "white" }}>Revision in expiry day of Index and Stock derivatives contractsF</a></li>
+          </ol>
+
+        </div>
+      </div>
+
+    </section>
   )
 }
