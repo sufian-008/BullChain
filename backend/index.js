c08ce8a612c93bae200ecba8c05ed82483b5bc2c
MD Abu Sufian <123016005+sufian-008@users.noreply.github.com>
2025-08-20 01:09:46 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ app.use(cookieParser());
 
 // Configure CORS to allow requests from frontend domains
 app.use(cors({
-    origin: ["https://bull-chain-tau.vercel.app/", "https://bull-chain-42g4.vercel.app"],
+    origin: ["https://bull-chain-tau.vercel.app", "https://bull-chain-42g4.vercel.app"],
     credentials: true, // Allow cookies to be sent with requests
 }));
 
